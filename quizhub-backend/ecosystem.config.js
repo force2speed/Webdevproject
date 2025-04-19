9c4d89eabdbeb5e402e8e59b30c646eaa320330b
dhruv dhoundiyal <dhruvdhoundiyal@gmail.com>
2025-04-20 00:41:13 +0530

    yaaaaa

--- quizhub-backend/ecosystem.config.js
+++ quizhub-backend/ecosystem.config.js
@@ -1,14 +1,13 @@
 module.exports = {
     apps: [
       {
-        name: 'quizhub-backend',
-        script: './server.js',
+        name: "quizhub-backend",
+        script: "server.js",
         env: {
-          NODE_ENV: 'production',
           PORT: 8081,
-          MONGODB_URI: 'mongodb+srv://dhruvdhoundiyal:XtNVFubmzJkaMFMy@quizhubcluster.k7gwuyt.mongodb.net/QUIZHUB?retryWrites=true&w=majority&appName=QUIZHUBCLUSTER'
+          NODE_ENV: "production",
+          MONGODB_URI: "mongodb+srv://dhruvdhoundiyal:XtNVFubmzJkaMFMy@quizhubcluster.k7gwuyt.mongodb.net/QUIZHUB?retryWrites=true&w=majority&appName=QUIZHUBCLUSTER"
         }
       }
     ]
   };
-  
